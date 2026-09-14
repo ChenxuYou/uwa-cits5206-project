ac620d992002f6a153e9ace2ffe7dcb51d181a22
Lam La <lamld@var-meta.com>
2026-09-14 18:12:04 +0800

    update: move validation test to different place and restore engine test

--- tests/CostingTool.Web.Tests/ValidationTests.cs
+++ tests/CostingTool.Web.Tests/ValidationTests.cs
@@ -1,12 +1,15 @@
 using CostingTool.Data;
 using CostingTool.Models;
 using CostingTool.Pages.Ric;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using System.Security.Claims;
 using Xunit;
 
-namespace CostingTool.Engine.Tests;
+namespace CostingTool.Web.Tests;
 
 public class ValidationTests
 {
@@ -43,32 +46,50 @@ public class ValidationTests
         return db;
     }
 
-    [Fact]
-    public async Task CapacityModelRejectsForecastUtilisationAboveCapacity()
+    private static CapacityModel CreateModel(CostingDbContext db, RicCycle cycle)
     {
-        await using var db = CreateDb();
+        var capability = cycle.Capabilities.Single();
         var model = new CapacityModel(db)
         {
-            CycleId = db.RicCycles.Single().Id,
+            CycleId = cycle.Id,
             Inputs =
             [
                 new CapacityModel.CapacityInput
                 {
-                    Id = db.RicCycles.Include(x => x.Capabilities).Single().Capabilities.Single().Id,
-                    MaximumCapacity = 1000m,
-                    UwaUse = 400m,
-                    ApfrUse = 350m,
-                    CommercialUse = 300m
+                    Id = capability.Id,
+                    MaximumCapacity = capability.MaximumCapacity,
+                    UwaUse = capability.ForecastUwaUse,
+                    ApfrUse = capability.ForecastApfrUse,
+                    CommercialUse = capability.ForecastCommercialUse
                 }
             ]
         };
 
-        var user = new System.Security.Claims.ClaimsPrincipal(new System.Security.Claims.ClaimsIdentity(
-            [new System.Security.Claims.Claim(CostingTool.Data.CurrentUser.UserNameClaim, "entry")],
+        var user = new ClaimsPrincipal(new ClaimsIdentity(
+            [new Claim(CurrentUser.UserNameClaim, "entry")],
             "TestAuth"));
-        model.PageContext = new Microsoft.AspNetCore.Mvc.RazorPages.PageContext
+
+        model.PageContext = new PageContext
         {
-            HttpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext { User = user }
+            HttpContext = new DefaultHttpContext { User = user }
+        };
+
+        return model;
+    }
+
+    [Fact]
+    public async Task CapacityModelRejectsForecastUtilisationAboveCapacity()
+    {
+        await using var db = CreateDb();
+        var cycle = db.RicCycles.Include(x => x.Capabilities).Single();
+        var model = CreateModel(db, cycle);
+        model.Inputs[0] = new CapacityModel.CapacityInput
+        {
+            Id = cycle.Capabilities.Single().Id,
+            MaximumCapacity = 1000m,
+            UwaUse = 400m,
+            ApfrUse = 350m,
+            CommercialUse = 300m
         };
 
         var result = await model.OnPostAsync();
@@ -78,34 +99,18 @@ public class ValidationTests
     }
 
     [Fact]
-    public async Task CapacityModelRejectsMissingForecastUtilisation()
+    public async Task CapacityModelRejectsZeroForecastUtilisation()
     {
         await using var db = CreateDb();
         var cycle = db.RicCycles.Include(x => x.Capabilities).Single();
-        var capability = cycle.Capabilities.Single();
-
-        var model = new CapacityModel(db)
+        var model = CreateModel(db, cycle);
+        model.Inputs[0] = new CapacityModel.CapacityInput
         {
-            CycleId = cycle.Id,
-            Inputs =
-            [
-                new CapacityModel.CapacityInput
-                {
-                    Id = capability.Id,
-                    MaximumCapacity = 1000m,
-                    UwaUse = 0m,
-                    ApfrUse = 0m,
-                    CommercialUse = 0m
-                }
-            ]
-        };
-
-        var user = new System.Security.Claims.ClaimsPrincipal(new System.Security.Claims.ClaimsIdentity(
-            [new System.Security.Claims.Claim(CostingTool.Data.CurrentUser.UserNameClaim, "entry")],
-            "TestAuth"));
-        model.PageContext = new Microsoft.AspNetCore.Mvc.RazorPages.PageContext
-        {
-            HttpContext = new Microsoft.AspNetCore.Http.DefaultHttpContext { User = user }
+            Id = cycle.Capabilities.Single().Id,
+            MaximumCapacity = 1000m,
+            UwaUse = 0m,
+            ApfrUse = 0m,
+            CommercialUse = 0m
         };
 
         var result = await model.OnPostAsync();
